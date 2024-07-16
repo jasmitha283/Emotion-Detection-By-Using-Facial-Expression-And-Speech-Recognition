d6a029747157c5090b11b1a8e1a61466760d3d3f
B.V Jasmitha <117642753+jasmitha283@users.noreply.github.com>
2024-07-16 09:51:32 +0530

    Add files via upload

---
+++ Multiple.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Multivaleus{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        double entryVal=input.nextDouble();
+     double[] array=new double[ARRAY_LIMIT];      
+
+  for(int entriesCounter=0; entriesCounter<array.length; entriesCounter++)
+  {
+     array[entriesCounter]=entryVal;
+     if(array[2]==0)
+        break;
+  }
+    }
+}
